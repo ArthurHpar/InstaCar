49e72190e257fdf090080d7caa071ca3541d50a0
Arthur de Paula Correa <arthur.correa@grupohpar.corp>
2024-01-23 15:12:50 -0400

    Biblioteca de Styles

---
+++ styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+const bg = StyleSheet.create({
+  gray50: {
+    backgroundColor: "#f9fafb"
+  },
+  gray100: {
+    backgroundColor: "#f3f4f6"
+  },
+  gray200: {
+    backgroundColor: "#e5e7eb"
+  },
+  gray300: {
+    backgroundColor: "#d1d5db"
+  },
+  gray400: {
+    backgroundColor: "#9ca3af"
+  },
+  gray500: {
+    backgroundColor: "#6b7280"
+  },
+  gray600: {
+    backgroundColor: "#4b5563"
+  },
+  gray700: {
+    backgroundColor: "#374151"
+  },
+  gray800: {
+    backgroundColor: "#1f2937"
+  },
+  gray900: {
+    backgroundColor: "#111827"
+  },
+  gray950: {
+    backgroundColor: "#030712"
+  }
+});
+
+export { bg };
